468765a7c9dcb5f9fe971dbf53c729ba6797480f
SamiFraca <sami.fraca@flat101.es>
2023-10-06 21:28:49 +0200

    feat: :lipstick: add basic header structure

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      maxWidth: {
+        nav: "75rem",
+      },
+      colors: {
+        "main-purple": "#9146DC",
+      },
+    },
+  },
+  plugins: [],
+};

abbeae2804c9030febefbb39d0beb8d5b7321a62
SamiFraca <sami.fraca@flat101.es>
2023-10-07 12:40:49 +0200

    add styles to project card react comp

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -2,6 +2,12 @@ import { useEffect, useState } from "react";
 type Props = {
   id: number;
 };
+const customStyles  = {
+'box-shadow':
+'0px 0px 20px 0px rgba(0, 0, 0, 0.05),0px 16px 30px 0px rgba(0, 0, 0, 0.05)'
+}
+
+
 export function ProjectCard({ id }: Props) {
   const [project, setProject] = useState<any>({});
 
@@ -14,7 +20,7 @@ export function ProjectCard({ id }: Props) {
   }, []);
 
   return (
-    <div className="w-[36.313rem] h-[10.875rem] bg-white projects-card rounded-[1.25rem] p-5 flex flex-col gap-2">
+    <div className="w-[36.313rem] h-[10.875rem] bg-white projects-card rounded-[1.25rem] p-5 flex flex-col gap-2 bg-white border border-gray-300 shadow-md p-4 transition-transform hover:shadow-lg hover:translate-y-[-3px] hover:border-main-purple hover:border-2 hover:cursor-pointer" >
       <div className="flex">
         <h3 className="font-bold text-[27px] text-ellipsis overflow-hidden whitespace-nowrap">
           {project.title}
@@ -35,3 +41,4 @@ export function ProjectCard({ id }: Props) {
     </div>
   );
 }
+
